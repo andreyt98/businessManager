030e6a1c2564d0dce92c737e24b97de8049a750e
andrey <andreytorrente@gmail.com>
2022-09-29 03:06:11 -0600

    add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <Link to="#" className="home-logo">
+        Business <i className="bi bi-exclude"></i> Manager
+      </Link>
+    </section>
+  );
+};
+
+export default Home;
