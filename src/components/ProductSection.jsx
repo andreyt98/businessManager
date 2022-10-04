37afa5d5ab00c1b46e745b59a7a4f6b067a6b514
andrey <andreytorrente@gmail.com>
2022-10-04 02:43:42 -0600

    add state for message and added to productSection component

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,26 +1,35 @@
 import Table from "./Table";
 import Modal from "./Modal";
-import { useState } from "react";
+import Message from "./Message";
+
+import { useState, useEffect } from "react";
 
 const ProductSection = () => {
-  const tableHeaders = ["Name", "Cost", "Quantity", ""]; 
-  let products = JSON.parse(localStorage.getItem("products")) || [];
+  const tableHeaders = ["Name", "Cost", "Quantity", ""];
 
+  const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products")) || []);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [message, setMessage] = useState({ active: false, type: null });
+
+  useEffect(() => {
+    setTimeout(() => {
+      
+      const timer = setTimeout(() => setMessage({ active: false, type: null }), 4000);
+      return () => clearTimeout(timer);
+    }, 4000);
+  }, [message]);
 
   return (
     <section id="product-section">
-      <h2>Products</h2>
-
+      <h2>Products</h2> {" "}
+ 
       <i className="bi bi-plus-circle-fill" id="add-product" onClick={() => setModalIsOpen(true)}></i>
 
-      <Table
-        tableHeaders={tableHeaders}
-        products={products}
-      />
+      <Table tableHeaders={tableHeaders} products={products} setProducts={setProducts} />
 
-      <Modal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} products={products}/>
+      <Modal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} products={products} setProducts={setProducts} message={message} setMessage={setMessage} />
       
+      {message && message.type === "success" ? <Message backgroundColor={"#D1E7DD"} textColor={"green"} iconClass={"bi bi-check-circle-fill"} text={"Product Added!"}  /> : null}
     </section>
   );
 };
