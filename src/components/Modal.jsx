974909db5c7415286c62ce02457f5ef07ff2bf02
andrey <andreytorrente@gmail.com>
2022-10-07 01:27:45 -0600

    add prop to pass different components

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,12 @@
 import PortalReactDOM from "react-dom";
 
-import Form from "./Form";
-
-const Modal = ({ modalIsOpen, setModalIsOpen, products, setProducts, message, setMessage,product, setProduct }) => {
+const Modal = ({ modalIsOpen, elementToAdd }) => {
   if (!modalIsOpen) return null;
 
   return PortalReactDOM.createPortal(
-    <Form  setModalIsOpen={setModalIsOpen} products={products} setProducts={setProducts} message={message} setMessage={setMessage} product={product} setProduct={setProduct}/>,
+    <div className="modal">
+      <div className="modal__content">{elementToAdd}</div>
+    </div>,
     document.querySelector("#portal-for-modal")
   );
 };
