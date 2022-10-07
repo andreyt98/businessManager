fe0eb13010643c7739b663df9041480589cdce80
andrey <andreytorrente@gmail.com>
2022-10-06 18:24:44 -0600

    change modal and form props

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,14 @@
-import PortalReactDOM from 'react-dom'
+import PortalReactDOM from "react-dom";
 
 import Form from "./Form";
 
-const Modal = ({modalIsOpen, setModalIsOpen, products}) => {
+const Modal = ({ modalIsOpen, setModalIsOpen, products, setProducts, message, setMessage,product, setProduct }) => {
+  if (!modalIsOpen) return null;
 
-    if(!modalIsOpen) return null;
-
-    return (
-        PortalReactDOM.createPortal( <Form setModalIsOpen={setModalIsOpen} products={products}/>,
-        document.querySelector("#portal-for-modal"))    
-    )
-}
+  return PortalReactDOM.createPortal(
+    <Form  setModalIsOpen={setModalIsOpen} products={products} setProducts={setProducts} message={message} setMessage={setMessage} product={product} setProduct={setProduct}/>,
+    document.querySelector("#portal-for-modal")
+  );
+};
 
 export default Modal;
