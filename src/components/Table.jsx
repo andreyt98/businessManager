9a3082c0b599c61a0e63ccbcb4b77ebef8d2cf71
andrey <andreytorrente@gmail.com>
2022-10-06 18:17:34 -0600

    add edit and delete functions and useEffect hook to manipulate local storage

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,32 +1,56 @@
-const Table = ({ tableHeaders, products }) => {
-  const headersArray = [];
-  const productsArray = [];
+import { useEffect } from "react";
+const Table = ({ tableHeaders, products, setProducts, setModalIsOpen, setProduct }) => {
 
-  tableHeaders.forEach((element) => {
-    headersArray.push(<th key={Math.random().toString(36) + 2000}>{element}</th>);
-  });
+  useEffect(() => {
+    localStorage.removeItem("products");
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
-  products.forEach((element) => {
-    productsArray.push(
-      <tr key={element.id} data-id={element.id}>
-        <td>{element.name}</td>
-        <td>{element.cost}</td>
-        <td>{element.quantity}</td>
-        <td>
-          <i className="bi bi-pencil-fill" id="edit-product"></i>
-          <i className="bi bi-trash3-fill" id="delete-product"></i>
-        </td>
-      </tr>
-    );
-  });
+  function handleDelete(id) {
+    products.forEach((element) => {
+      if (id === element.id) {
+        const newArray = products.filter((e) => e.id !== element.id);
 
+        setProducts(newArray);
+      }
+    });
+  }
+
+  function handleEdit(id){
+
+    products.forEach((product) => {
+      if(product.id===id){
+        setModalIsOpen(true);
+        setProduct(product)
+      }
+    })
+
+  }
   return (
     <div className="table-container">
       <table>
         <thead>
-          <tr>{headersArray}</tr>
+          <tr>
+            {tableHeaders.map((element) => {
+              return <th key={Math.random().toString(36) + 2000}>{element}</th>;
+            })}
+          </tr>
         </thead>
-        <tbody>{productsArray}</tbody>
+        <tbody>
+          {products.map((element) => {
+            return (
+              <tr key={element.id} data-id={element.id}>
+                <td>{element.name}</td>
+                <td>{element.cost}</td>
+                <td>{element.quantity}</td>
+                <td>
+                  <i className="bi bi-pencil-fill" id="edit-product" onClick={()=>{handleEdit(element.id)}}></i>
+                  <i className="bi bi-trash3-fill" id="delete-product" onClick={() => handleDelete(element.id)}></i>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
