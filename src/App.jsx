3ee4622b0b3e4f0d980b94b60175e6e3071b1f97
andrey <andreytorrente@gmail.com>
2022-10-16 01:50:05 -0600

    add product state to app component to pass it to other section components

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products")) || []);
+  const [product, setProduct] = useState({});
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  
   return (
     <div className="App">
       <BrowserRouter>
@@ -20,8 +22,8 @@ function App() {
         <MobileNavbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/products" element={<ProductSection modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}></Route>
-          <Route path="/sale" element={<SaleSection modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}></Route>
+          <Route path="/products" element={<ProductSection products={products} setProducts={setProducts} product={product} setProduct={setProduct} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}></Route>
+          <Route path="/sale" element={<SaleSection products={products} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
