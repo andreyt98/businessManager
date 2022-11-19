74d9d6a11da4a15b5893951849f01ef9133b5512
andrey <andreytorrente@gmail.com>
2022-11-19 14:03:39 -0600

    add products via props and add conditional rendering of message

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,36 +1,38 @@
 import Table from "./Table";
 import Modal from "./Modal";
 import Message from "./Message";
+import Form from "./Form";
 
 import { useState, useEffect } from "react";
 
-const ProductSection = ({modalIsOpen,setModalIsOpen}) => {
+const ProductSection = ({ products, setProducts, product, setProduct, modalIsOpen, setModalIsOpen }) => {
   const tableHeaders = ["Name", "Cost", "Quantity", ""];
-
-  const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products")) || []);
-  const [product, setProduct] = useState({});
 
   const [message, setMessage] = useState({ active: false, type: null });
 
   useEffect(() => {
     setTimeout(() => {
-      
-      const timer = setTimeout(() => setMessage({ active: false, type: null }), 4000);
+      const timer = setTimeout(() => setMessage({ active: false, type: null }), 2500);
       return () => clearTimeout(timer);
-    }, 4000);
-  }, [message]);
+    }, 2500);
+  }, [ProductSection]);
 
   return (
     <section id="product-section">
-      <h2>Products</h2> {" "}
- 
-      <i className="bi bi-plus-circle-fill section-btn " id="add-product" onClick={() => setModalIsOpen(true)}></i>
-
-      <Table tableHeaders={tableHeaders} products={products} setProducts={setProducts} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} setProduct={setProduct}/>
-
-      <Modal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} products={products} setProducts={setProducts} message={message} setMessage={setMessage} product={product} setProduct={setProduct}/>
+      {message && message.type === "success" ? <Message backgroundColor={"#D1E7DD"} textColor={"green"} iconClass={"bi bi-check-circle-fill"} text={"Product Added!"} /> : null}
       
-      {message && message.type === "success" ? <Message backgroundColor={"#D1E7DD"} textColor={"green"} iconClass={"bi bi-check-circle-fill"}  text={"Product Added!"}  /> : null}
+      <h2>Products</h2> <i className="bi bi-plus-circle-fill section-btn " id="add-product" onClick={() => setModalIsOpen(true)}></i>
+      
+      {Object.keys(products).length > 0 ? (
+        <Table tableHeaders={tableHeaders} products={products} setProducts={setProducts} setProduct={setProduct} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
+      ) : (
+        <Message backgroundColor={"#0059ff"} textColor={"#ffffff"} iconClass={"bi bi-exclamation-circle-fill"} text={"No products added yet"} />
+      )}
+     
+      <Modal
+        modalIsOpen={modalIsOpen}
+        elementToAdd={<Form setModalIsOpen={setModalIsOpen} products={products} setProducts={setProducts} message={message} setMessage={setMessage} product={product} setProduct={setProduct} />}
+      />
     </section>
   );
 };
