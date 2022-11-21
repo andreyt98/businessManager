7ec9e2e990e3e3f8e3d6db6cfd2a8711e7d88d41
andrey <andreytorrente@gmail.com>
2022-11-21 14:52:34 -0600

    use context API to avoid some prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SaleSection from "./components/SaleSection";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useState } from "react";
+import { Context } from "./context/Context";
 
 function App() {
   const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products")) || []);
@@ -17,18 +18,34 @@ function App() {
   const [sales, setSales] = useState(JSON.parse(localStorage.getItem("sales")) || []);
   const [sale, setSale] = useState({});
 
+  const [successMessage, setSuccessMessage] = useState(false);
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <DesktopNavbar />
-        <MobileNavbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/products" element={<ProductSection products={products} setProducts={setProducts} product={product} setProduct={setProduct} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}></Route>
-          <Route path="/sale" element={<SaleSection products={products} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Context.Provider
+        value={{
+          products,
+          setProducts,
+          product,
+          setProduct,
+          successMessage,
+          setSuccessMessage,
+          modalIsOpen,
+          setModalIsOpen,
+        }}
+      >
+        <BrowserRouter>
+          <DesktopNavbar />
+          <MobileNavbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/products" element={<ProductSection />}></Route>
+            <Route path="/sale" element={<SaleSection products={products} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
     </div>
   );
 }
